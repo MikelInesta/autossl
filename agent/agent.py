b8262655088dd1e6a4cd2c0dcd92f6f677a1c35e
MikelInestaUsal <mikel_inesta@usal.es>
2024-01-29 12:27:12 +0100

    Agent using file search

--- agent/agent.py
+++ agent/agent.py
@@ -3,6 +3,7 @@ import requests
 from autossl import Agent
 import os # To check for root privileges
 
+""""" This lines are commented out because for now we are using file search instead of service search using netstat
 # Check for root privileges, for netstat to show full process information (pid and name are exclusive to owner)
 if os.geteuid() != 0:
   exit("Root privileges are required to access server information.\n\
@@ -11,18 +12,41 @@ Please try again as root or using 'sudo'. Exiting.")
 agent = Agent()
 agent.findActiveServers()
 
+
+"""""
+
+# Initialize the agent
+agent = Agent()
+
+searchRoot = "/etc"
+
+# Search the directory /etc for configuration files related to nginx and apache
+agent.findDir("nginx", searchRoot)
+agent.findDir("apache2", searchRoot)
+agent.findDir("apache", searchRoot)
+agent.findDir("httpd", searchRoot)
+
 print(f'Found {agent.servers}')
+
+# Add the current server IP for identification
+ip = requests.get('https://api.ipify.org').content.decode('utf8')
+jsonDict = {"ip":ip, "servers": agent.servers}
+
 # Convert the servers list to JSON
-serversJson = json.dumps(agent.servers)
+serversJson = json.dumps(jsonDict)
 
 # Send the JSON to the node backend
-url = "http://autossl.mikelinesta.com/node/sendStatus"
+
+
+url = "https://autossl.mikelinesta.com/backend/update"
 headers = {"Content-Type": "application/json"}
-response = requests.post(url, data=serversJson, headers=headers)
 
-if response.status_code == 200:
+try:
+  response = requests.post(url, data=serversJson, headers=headers)
+  if response.status_code == 200:
     print("Data sent")
-else:
-    print("Error sending data")
+  else:
+      print("Error sending data")
+except Exception:
+  print(f'Error establishing a connection with {url}')
 
-###coment
--- agent/autossl.py
+++ agent/autossl.py
@@ -1,25 +1,26 @@
-import subprocess # To run shell commands
-#import re # Regex parsing 
+import subprocess # To run shell commands.
+import os # For file search
 
 class Agent:
     def __init__(self):
         self.servers = []
 
-    def findActiveServers(self):
+    # Method that uses netstat to find active internet services by name (apache or nginx) or common ports (80, 8080, 443, 8443)
+    def findActiveServicesThroughNetstat(self):
         # Shell command to find all servers listening on ports 80, 8080, 443, 8443 or 
-        # containing the names apache (or httpd) or nginx
+        # containing the names apache (or httpd) or nginx.
         command = "sudo netstat -tnlp  | grep 'apache\|nginx\|httpd\|:80\|:8080\|:443\|:8443' | awk '{print $4,$7}'"
         result = subprocess.run(command, shell=True, capture_output=True, text=True)
         output = result.stdout.strip()
 
         lines = output.split('\n')
         for line in lines:
-            parts = line.split(' ') #Splits the line into port and pid/name
-            port = parts[0].split(':')[1] #Splits the port from the ip
-            pid = parts[1].split('/')[0] #Splits the pid from the name
-            name = parts[1].split('/')[1].rstrip(':') #Splits the name from the pid and removes the final colon
+            parts = line.split(' ') #S plits the line into port and pid/name.
+            port = parts[0].split(':')[1] #S plits the port from the ip.
+            pid = parts[1].split('/')[0] #S plits the pid from the name.
+            name = parts[1].split('/')[1].rstrip(':') #S plits the name from the pid and removes the final colon.
             print(f'Port:{port}, PID: {pid}, Name: {name}')
-            # Check to find wether a server with the same name and pid is already considered
+            # Check to find wether a server with the same name and pid is already considered.
             for server in self.servers:
                 if server['name'] == name and server['pid'] == pid:
                     if port != '' and port not in server['ports']:
@@ -28,3 +29,21 @@ class Agent:
             else:
                 self.servers.append({'name': name, 'pid': pid, 'ports': [port]})
 
+    # Method that uses the file system to find if apache or nginx configuration directories exist.
+    # Uses walk to search entire depth
+    def findDirWalk(self, dirName, searchPath):
+        result = []
+        for root, dirs, files in os.walk(searchPath):
+            if dirName in dirs:
+                result.append(os.path.join(root, dirName))
+        if result:
+            result.insert(0, dirName) # We add the server name as the first element
+            self.servers.append(result)
+        return result
+    
+    # Method that just searches a given directory for a specific directory in it
+    def findDir(self, dirName, searchDir):
+        for item in os.listdir(searchDir):
+            if os.path.isdir(os.path.join(searchDir, item)) and item == dirName:
+                self.servers.append(os.path.join(searchDir, item))
+    
