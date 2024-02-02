fa0c1f71cdc6af1ce7a435c2c96d7a6e4c28d6f9
MikelInestaUsal <mikel_inesta@usal.es>
2024-02-02 19:27:37 +0100

    blank slate

--- autossl/client/src/App.js
+++ autossl/client/src/App.js
@@ -1,18 +1,11 @@
 // Simple application template for getting web applications installed in a given server
 // Pending implementation
 
-function ObtenerServidoresWeb(){
-  return (
-    <button>Mostrar</button>
-  );
-}
-
 export default function MyApp() {
   return (
     <div>
       <h1>AutoSSL</h1>
-      <h2>Obtener servidores web instalados</h2>
-      <ObtenerServidoresWeb />
+      <h2>Frontend Pendiente...</h2>
     </div>
   );
 }
