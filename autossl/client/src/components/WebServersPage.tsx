6278eda0233b0145362f2afdea512d5ee5784b8f
MikelInestaUsal <mikel_inesta@usal.es>
2024-02-04 10:36:49 -0600

    Component to handle showing web servers given a server name

---
+++ autossl/client/src/components/WebServersPage.tsx
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+//import React, { FC } from "react"; // For declaring component type
+
+/*Input for server name, logic for requesting the backend and display of returned list*/
+export default function WebServersPage() {
+  const [serverName, setServerName] = useState("");
+  const [webServers, setWebServers] = useState([]);
+  const [error, setError] = useState("");
+
+  // Backend query for the web_server list
+  const fetchData = async () => {
+    try {
+      const response = await fetch(
+        "https://autossl.mikelinesta.com/backend/web-servers/" + serverName,
+        {
+          method: "GET",
+          headers: new Headers({ "Content-type": "application/json" }), // Not really sending json but it gives me cors error if i dont
+          mode: "cors",
+        }
+      );
+      if (!response.ok) {
+        const errorData = await response.json();
+        setError(errorData.error);
+      } else {
+        const data = await response.json();
+        setWebServers(data);
+        setError("");
+      }
+    } catch (error) {
+      console.error("Error fetching data: ", error);
+    }
+  };
+
+  // Event handler for the server name input.
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setServerName(e.target.value);
+  };
+
+  // Event handler for the button.
+  const handleButtonClick = () => {
+    setWebServers([]); // Clean the previous result
+    fetchData();
+  };
+
+  return (
+    <div className="container text-center">
+      <input
+        className="form-control mb-3 w-25 mx-auto"
+        type="text"
+        id="serverName"
+        value={serverName}
+        onChange={handleInputChange}
+        placeholder="Server Name"
+      />
+      <button onClick={handleButtonClick} className="btn btn-primary">
+        Get Web Servers {serverName && " for server: " + serverName}
+      </button>
+
+      {error && <div className="text-danger mt-3">{error}</div>}
+
+      <ul className="list-group mt-3">
+        {webServers.map((server: any) => (
+          <li key={server.web_server_id} className="list-group-item">
+            <h5>{server.web_server_name}</h5>
+            <p>{server.configuration_path}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
