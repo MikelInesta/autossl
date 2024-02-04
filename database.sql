a770b112f97669c7fa26bf817aed2bde3cdb5304
MikelInestaUsal <mikel_inesta@usal.es>
2024-02-04 09:15:23 -0600

    fixed domain_name type, added unique key pair (user_id, server_name) to server

--- database.sql
+++ database.sql
@@ -11,72 +11,63 @@ CREATE DATABASE IF NOT EXISTS autossl
 
 USE autossl;
 
-CREATE TABLE user (
-	user_id int NOT NULL AUTO_INCREMENT,
-	username varchar(36) NOT NULL,
-	email varchar(255) NOT NULL,
-	password varchar(43) NOT NULL,
-	PRIMARY KEY (user_id)
+CREATE TABLE `users` (
+    user_id int NOT NULL AUTO_INCREMENT,
+    username varchar(36) NOT NULL,
+    email varchar(255) NOT NULL,
+    password varchar(43) NOT NULL,
+    PRIMARY KEY (user_id)
 ) CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-INSERT INTO user (username, email, password) values ("test", "test@test.test", "test");
+INSERT INTO `users` (username, email, password) values ("test", "test@test.test", "test");
 
 CREATE TABLE server (
-	server_id int NOT NULL AUTO_INCREMENT,
-	user_id int NOT NULL,
-	ip varchar(45) NOT NULL,
-	server_name varchar(36) NOT NULL,
-	PRIMARY KEY (server_id)
+    server_id int NOT NULL AUTO_INCREMENT,
+    user_id int NOT NULL,
+    ip varchar(45) NOT NULL,
+    server_name varchar(36) NOT NULL,
+    PRIMARY KEY (server_id),
+    UNIQUE KEY unique_server_user (server_name, user_id)
 ) CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-INSERT INTO server (user_id, ip, server_name) values (1, "0.0.0.0", "test");
-
 CREATE TABLE web_server (
-	web_server_id int NOT NULL AUTO_INCREMENT,
-	server_id int NOT NULL,
-	web_server_name VARCHAR(255) NOT NULL,
-	configuration_path VARCHAR(4096) NOT NULL,
-	PRIMARY KEY (web_server_id)
+    web_server_id int NOT NULL AUTO_INCREMENT,
+    server_id int NOT NULL,
+    web_server_name VARCHAR(255) NOT NULL,
+    configuration_path VARCHAR(4096) NOT NULL,
+    PRIMARY KEY (web_server_id)
 ) CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 CREATE TABLE domain (
-	domain_id int NOT NULL AUTO_INCREMENT,
-	web_server_id int NOT NULL,
-	domain_name int(253) NOT NULL,
-	PRIMARY KEY (domain_id)
+    domain_id int NOT NULL AUTO_INCREMENT,
+    web_server_id int NOT NULL,
+    domain_name varchar(253) NOT NULL,
+    PRIMARY KEY (domain_id)
 ) CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 CREATE TABLE certificate (
-	certificate_id int NOT NULL AUTO_INCREMENT,
-	provider varchar(36) NOT NULL,
-	expiration_date DATE NOT NULL,
-	PRIMARY KEY (certificate_id)
+    certificate_id int NOT NULL AUTO_INCREMENT,
+    provider varchar(36) NOT NULL,
+    expiration_date DATE NOT NULL,
+    PRIMARY KEY (certificate_id)
 ) CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-CREATE TABLE certificate_domain(
-	certificate_id int NOT NULL,
-	domain_id int NOT NULL
+CREATE TABLE certificate_domain (
+    certificate_id int NOT NULL,
+    domain_id int NOT NULL
 ) CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- foreign key constraints
 
 ALTER TABLE server
-	ADD CONSTRAINT server_ibfk_1 FOREIGN KEY (user_id) REFERENCES user (user_id) ON DELETE CASCADE ON UPDATE CASCADE;
+    ADD CONSTRAINT server_ibfk_1 FOREIGN KEY (user_id) REFERENCES `users` (user_id) ON DELETE CASCADE ON UPDATE CASCADE;
 
 ALTER TABLE web_server
-	ADD CONSTRAINT web_server_ibfk_1 FOREIGN KEY (server_id) REFERENCES server (server_id) ON DELETE CASCADE ON UPDATE CASCADE;
+    ADD CONSTRAINT web_server_ibfk_1 FOREIGN KEY (server_id) REFERENCES server (server_id) ON DELETE CASCADE ON UPDATE CASCADE;
 
 ALTER TABLE domain
-	ADD CONSTRAINT domain_ibfk_1 FOREIGN KEY (web_server_id) REFERENCES web_server (web_server_id) ON DELETE CASCADE ON UPDATE CASCADE;
+    ADD CONSTRAINT domain_ibfk_1 FOREIGN KEY (web_server_id) REFERENCES web_server (web_server_id) ON DELETE CASCADE ON UPDATE CASCADE;
 
 ALTER TABLE certificate_domain
-	ADD CONSTRAINT certificate_domain_ibfk_1 FOREIGN KEY (certificate_id) REFERENCES certificate (certificate_id) ON DELETE CASCADE ON UPDATE CASCADE,
-	ADD CONSTRAINT certificate_domain_ibfk_2 FOREIGN KEY (domain_id) REFERENCES domain (domain_id) ON DELETE CASCADE ON UPDATE CASCADE;
-
-	
-
-
-
-
-
-
+    ADD CONSTRAINT certificate_domain_ibfk_1 FOREIGN KEY (certificate_id) REFERENCES certificate (certificate_id) ON DELETE CASCADE ON UPDATE CASCADE,
+    ADD CONSTRAINT certificate_domain_ibfk_2 FOREIGN KEY (domain_id) REFERENCES domain (domain_id) ON DELETE CASCADE ON UPDATE CASCADE;
