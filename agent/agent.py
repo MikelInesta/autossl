66023d719643ffc26a73565206e9a237af595105
MikelInestaUsal <mikel_inesta@usal.es>
2024-02-13 15:58:38 -0600

    updated api route

--- agent/agent.py
+++ agent/agent.py
@@ -1,52 +1,41 @@
 import json
-import requests
-from autossl import Agent
-import os # To check for root privileges
+import requests #external (pip install requests)
+from helper import FileExplorer
 
-""""" This lines are commented out because for now we are using file search instead of service search using netstat
-# Check for root privileges, for netstat to show full process information (pid and name are exclusive to owner)
-if os.geteuid() != 0:
-  exit("Root privileges are required to access server information.\n\
-Please try again as root or using 'sudo'. Exiting.")
+def updateWebServers():
 
-agent = Agent()
-agent.findActiveServers()
+  # Initialize the agent
+  fe = FileExplorer()
 
+  searchRoot = "/etc"
 
-"""""
+  # Search the directory /etc for configuration files related to nginx and apache
+  fe.findDir("nginx", searchRoot)
+  fe.findDir("apache2", searchRoot)
+  fe.findDir("apache", searchRoot)
+  fe.findDir("httpd", searchRoot)
 
-# Initialize the agent
-agent = Agent()
+  print(f'Found {fe.servers}')
 
-searchRoot = "/etc"
+  # Add the current server IP for identification
+  ip = requests.get('https://api.ipify.org').content.decode('utf8')
+  jsonDict = {"ip":ip, "servers": fe.servers}
 
-# Search the directory /etc for configuration files related to nginx and apache
-agent.findDir("nginx", searchRoot)
-agent.findDir("apache2", searchRoot)
-agent.findDir("apache", searchRoot)
-agent.findDir("httpd", searchRoot)
+  # Convert the servers list to JSON
+  serversJson = json.dumps(jsonDict)
 
-print(f'Found {agent.servers}')
-
-# Add the current server IP for identification
-ip = requests.get('https://api.ipify.org').content.decode('utf8')
-jsonDict = {"ip":ip, "servers": agent.servers}
-
-# Convert the servers list to JSON
-serversJson = json.dumps(jsonDict)
-
-# Send the JSON to the node backend
+  # Send the JSON to the node backend
 
 
-url = "https://autossl.mikelinesta.com/backend/update"
-headers = {"Content-Type": "application/json"}
+  url = "https://autossl.mikelinesta.com/api/agents/update/web-servers"
+  headers = {"Content-Type": "application/json"}
 
-try:
-  response = requests.post(url, data=serversJson, headers=headers)
-  if response.status_code == 200:
-    print("Data sent")
-  else:
-      print("Error updating data:", response)
-except Exception:
-  print(f'Error establishing a connection with {url}')
+  try:
+    response = requests.post(url, data=serversJson, headers=headers)
+    if response.status_code == 200:
+      print(response);
+    else:
+        print("Error updating data:", response)
+  except Exception:
+    print(f'Error establishing a connection with {url}')
 
