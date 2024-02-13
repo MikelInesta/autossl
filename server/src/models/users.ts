7a8d85a668bd57fc4b0e46b5c9837ccbb9d67690
MikelInestaUsal <mikel_inesta@usal.es>
2024-02-13 15:58:21 -0600

    user mongoose model

---
+++ server/src/models/users.ts
@@ -0,0 +1,39 @@
+import { Schema, model, Types } from "mongoose";
+import { IServer, Server } from "./servers";
+
+interface IUser {
+  user_name: string;
+  email: string;
+  password: string;
+  servers?: Types.DocumentArray<IServer>;
+}
+
+const schema = new Schema<IUser>({
+  user_name: {
+    type: "string",
+    required: true,
+    max: 50,
+  },
+  email: {
+    type: "string",
+    required: true,
+    max: 254,
+    unique: true,
+  },
+  password: {
+    type: "string",
+    required: true,
+    max: 43,
+  },
+  servers: {
+    type: "array",
+    items: {
+      type: Schema.Types.ObjectId,
+      ref: Server,
+    },
+  },
+});
+
+const User = model<IUser>("User", schema);
+
+export { User, IUser };
