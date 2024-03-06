8ebca0014bc7219097357680891d5c00593270cd
MikelInestaUsal <mikel_inesta@usal.es>
2024-03-06 11:40:50 -0600

    Certificate model schema with more info

--- server/src/models/certificates.ts
+++ server/src/models/certificates.ts
@@ -2,62 +2,100 @@ import { Schema, model, Types } from "mongoose";
 
 interface ICertificate extends Document {
   _id: Types.ObjectId;
-  ca_chain_path: string;
-  is_old: boolean;
+  directory_path: string;
+  subject: {
+    common_name: string;
+    organization: string;
+    organizational_unit: string;
+  };
+  issuer: {
+    common_name: string;
+    organization: string;
+    organizational_unit: string;
+  };
+  validity: {
+    not_before: Date;
+    not_after: Date;
+  };
+  public_key: {
+    algorithm: string;
+    size: number;
+  };
+  signature_algorithm: string;
+  serial_number: string;
 }
 
 const schema = new Schema<ICertificate>({
-  ca_chain_path: {
+  directory_path: {
     type: "string",
     required: true,
   },
-  is_old: {
-    type: "boolean",
-    required: true,
-    default: false,
+  subject: {
+    common_name: {
+      type: "string",
+      required: true,
+      max: 50,
+    },
+    organization: {
+      type: "string",
+      required: true,
+      max: 50,
+    },
+    organizational_unit: {
+      type: "string",
+      required: true,
+      max: 50,
+    },
   },
-});
-
-/*
-interface ICertificate extends Document {
-  ca_name: string;
-  ca_signature: string;
-  issue_date: Date;
-  expiration_date: Date;
-  public_key: string;
-  version: string;
-}
-
-const schema = new Schema<ICertificate>({
-  ca_name: {
+  issuer: {
+    common_name: {
+      type: "string",
+      required: true,
+      max: 50,
+    },
+    organization: {
+      type: "string",
+      required: true,
+      max: 50,
+    },
+    organizational_unit: {
+      type: "string",
+      required: true,
+      max: 50,
+    },
+  },
+  validity: {
+    not_before: {
+      type: "date",
+      required: true,
+    },
+    not_after: {
+      type: "date",
+      required: true,
+    },
+  },
+  public_key: {
+    algorithm: {
+      type: "string",
+      required: true,
+      max: 50,
+    },
+    size: {
+      type: "number",
+      required: true,
+    },
+  },
+  signature_algorithm: {
     type: "string",
     required: true,
     max: 50,
   },
-  ca_signature: {
+  serial_number: {
     type: "string",
     required: true,
-  },
-  issue_date: {
-    type: "date",
-    required: true,
-  },
-  expiration_date: {
-    type: "date",
-    required: true,
-  },
-  public_key: {
-    type: "string",
-    required: true,
-  },
-  version: {
-    type: "string",
-    required: true,
-    max: 10,
+    max: 50,
   },
 });
-
-*/
 
 const Certificate = model<ICertificate>("Certificate", schema);
 
