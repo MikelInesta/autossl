fb0092acf1214e5343aac290b201a4ee45c22c76
MikelInestaUsal <mikel_inesta@usal.es>
2024-03-06 11:41:34 -0600

    removed old endpoint

--- server/src/controllers/web_server.ts
+++ server/src/controllers/web_server.ts
@@ -1,6 +1,5 @@
 import { WebServer } from "../models/web_servers";
 import { Server } from "../models/servers";
-import { createNewServer } from "./server";
 
 export const getWebServersByName = async (name: string) => {
   // Find the server with the given ip
@@ -18,26 +17,4 @@ export const getWebServersByName = async (name: string) => {
   });
   console.log(webServers);
   return webServers;
-};
-
-export const updateWebServers = async (ip: string, webServers: String[]) => {
-  // Create the new web servers objects to be added to the server
-  const newWebServers = webServers.map((webServer) => {
-    // Create a new web server and save it to the database
-    return new WebServer({
-      web_server_name: webServer.slice(webServer.lastIndexOf("/")),
-      configuration_path: webServer,
-    });
-  });
-
-  // Find the server with the given ip and update the webservers
-  const server = await Server.findOneAndUpdate(
-    { server_ip: ip },
-    { web_servers: newWebServers }
-  );
-  if (!server) {
-    // If the server does not exist, create a new one with the given server ip and web servers
-    //Linux is hard coded right now, but it should be obtained from the agent
-    await createNewServer(ip, "Linux", newWebServers);
-  }
 };
