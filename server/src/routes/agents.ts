c08c6d71b4b03020a0e376e67dac498742215a3e
MikelInestaUsal <mikel_inesta@usal.es>
2024-03-06 11:41:14 -0600

    removed old route

--- server/src/routes/agents.ts
+++ server/src/routes/agents.ts
@@ -1,26 +1,18 @@
 import express from "express";
-import { updateWebServers } from "../controllers/web_server";
+import { Server, IServer } from "../models/servers";
+import { WebServer, IWebServer } from "../models/web_servers";
+import { VirtualHost, IVirtualHost } from "../models/virtual_hosts";
+import { Certificate, ICertificate } from "../models/certificates";
 
 const agentRouter = express.Router();
 
-agentRouter.get("/test", (req, res) => {
-  res.send("Hello world!");
-});
-
 // Endpoint for agents to update web servers
-agentRouter.post("/update/web-servers", async (req, res) => {
-  // Get the list of web servers from the agent
-  const webServers = req.body.servers;
-  const ip = req.body.ip;
+agentRouter.post("/update", async (req, res) => {
+  //Receive the json data from the agent
+  const data = req.body;
+  console.log(data);
 
-  try {
-    await updateWebServers(ip, webServers);
-    res.status(200).send("Web servers updated succesfully!");
-    console.log("Web servers updated succesfully!");
-  } catch (err) {
-    console.log(err);
-    res.status(500).send("Internal server error");
-  }
+  res.sendStatus(200);
 });
 
 export default agentRouter;
