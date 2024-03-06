e5a10d9f7cb50d855235ed18f04ce9531a34a259
MikelInestaUsal <mikel_inesta@usal.es>
2024-03-06 03:24:34 -0600

    initial approach to deleting the entire server in the db and recreating

--- server/src/controllers/server.ts
+++ server/src/controllers/server.ts
@@ -1,21 +1,70 @@
-import { IWebServer } from "../models/web_servers";
-import { Server } from "../models/servers";
+import { IWebServer, WebServer } from "../models/web_servers";
+import { IServer, Server } from "../models/servers";
+import { Mongoose, Types } from "mongoose";
+import { VirtualHost } from "../models/virtual_hosts";
+import { Certificate } from "../models/certificates";
 
-export const createNewServer = async (
-  ip: string,
-  os: string,
-  newWebServers: IWebServer[]
-) => {
-  //Get the name of the last recorded server and add 1 to it
-  const lastServer = await Server.findOne();
-  const lastVal = lastServer ? lastServer.server_name.split("-")[1] : "0";
-  const newServer = new Server({
-    server_name: `Server-${parseInt(lastVal) + 1}`,
-    server_ip: ip,
-    operating_system: os,
-    web_servers: newWebServers,
-  });
+const ReplaceServer = async (
+  server_name: string,
+  server_ip: string,
+  operating_system: string
+): Promise<IServer> => {
+  try {
+    // Attempt to find the server
+    const server = await Server.findOne({
+      server_ip: server_ip,
+    }).exec();
 
-  newServer.save();
-  return newServer;
+    if (server) {
+      console.log("Server found:", server);
+      return server;
+    } else {
+      // Server was not found, creating a new one
+      const newServer = new Server({
+        server_name,
+        server_ip,
+        operating_system,
+      });
+      await newServer.save();
+      console.log("New server created:", newServer);
+      return newServer;
+    }
+  } catch (e: any) {
+    throw new Error(`FindOrCreateServer failed: ${e.message}`);
+  }
 };
+
+const deleteServer = async (server: Partial<IServer>): Promise<boolean> => {
+  if (server.web_servers) {
+    try {
+      // Find the virtualhosts for each web_server and delete them
+      for (const web_server of server.web_servers) {
+        /* Find the certificate for every virtualhost and set its is_old field to true
+            I don´t delete certificates when updating for logging*/
+        const virtual_hosts = await VirtualHost.find({
+          web_server_id: web_server._id,
+        });
+        for (const virtual_host of virtual_hosts) {
+          await Certificate.findByIdAndUpdate(virtual_host.certificate, {
+            is_old: true,
+          });
+        }
+      }
+      return true;
+    } catch (e: any) {
+      console.log(`deleteServer failed: ${e.message}`);
+      return false;
+    }
+  } else {
+    // Delete the server
+    try {
+      await Server.findByIdAndDelete(server._id);
+      return true;
+    } catch (e: any) {
+      console.log(`deleteServer failed to delete the server: ${e.message}`);
+      return false;
+    }
+  }
+};
+
+export { ReplaceServer };
