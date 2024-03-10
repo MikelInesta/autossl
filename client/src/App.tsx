dab1a6e4b050b8366e90a1e6466945e05f1b3a46
MikelInestaUsal <mikel_inesta@usal.es>
2024-03-10 17:41:13 -0600

    Created a Servers page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,11 @@
 import "./App.css";
-import WebServersPage from "./components/WebServersPage";
+import ServersPage from "./pages/ServersPage";
 
 export default function App() {
   return (
     <div className="border">
-      <h1 className="text-primary">Autossl</h1>
-      <p>Input the name of a server to get its installed web servers</p>
-      <WebServersPage />
+      <h1 className="text-primary">AutoSSL</h1>
+      <ServersPage />
     </div>
   );
 }
