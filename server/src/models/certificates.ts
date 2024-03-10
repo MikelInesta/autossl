d001e6a05e2fe5c38983bd15b409ad5fb53844e8
MikelInestaUsal <mikel_inesta@usal.es>
2024-03-10 13:30:29 -0600

    Alligned certificate model with parsed data

--- server/src/models/certificates.ts
+++ server/src/models/certificates.ts
@@ -2,92 +2,57 @@ import { Schema, model, Types } from "mongoose";
 
 interface ICertificate extends Document {
   _id: Types.ObjectId;
-  directory_path: string;
   subject: {
     common_name: string;
     organization: string;
     organizational_unit: string;
+    country: string;
+    state: string;
+    locality: string;
   };
   issuer: {
     common_name: string;
     organization: string;
     organizational_unit: string;
+    country: string;
+    state: string;
+    locality: string;
   };
-  validity: {
-    not_before: Date;
-    not_after: Date;
-  };
-  public_key: {
-    algorithm: string;
-    size: number;
-    key: string;
-  };
-  signature_algorithm: string;
+  has_expired: boolean;
+  not_after: Date;
+  not_before: Date;
   serial_number: string;
+  serial_number_hex: string;
+  signature_algorithm: string;
+  version: number;
+  public_key_length: number;
 }
 
 const schema = new Schema<ICertificate>({
-  directory_path: {
-    type: "string",
-    required: true,
-  },
   subject: {
-    common_name: {
-      type: "string",
-      max: 50,
-    },
-    organization: {
-      type: "string",
-      max: 50,
-    },
-    organizational_unit: {
-      type: "string",
-      max: 50,
-    },
+    common_name: { type: String },
+    organization: { type: String },
+    organizational_unit: { type: String },
+    country: { type: String },
+    state: { type: String },
+    locality: { type: String },
   },
   issuer: {
-    common_name: {
-      type: "string",
-      max: 50,
-    },
-    organization: {
-      type: "string",
-      max: 50,
-    },
-    organizational_unit: {
-      type: "string",
-      max: 50,
-    },
+    common_name: { type: String },
+    organization: { type: String },
+    organizational_unit: { type: String },
+    country: { type: String },
+    state: { type: String },
+    locality: { type: String },
   },
-  validity: {
-    not_before: {
-      type: "date",
-    },
-    not_after: {
-      type: "date",
-    },
-  },
-  public_key: {
-    algorithm: {
-      type: "string",
-
-      max: 50,
-    },
-    size: {
-      type: "number",
-    },
-    key: {
-      type: "string",
-    },
-  },
-  signature_algorithm: {
-    type: "string",
-    max: 50,
-  },
-  serial_number: {
-    type: "string",
-    max: 100,
-  },
+  has_expired: { type: Boolean, required: true },
+  not_after: { type: Date, required: true },
+  not_before: { type: Date, required: true },
+  serial_number: { type: String, required: true },
+  serial_number_hex: { type: String, required: true },
+  signature_algorithm: { type: String, required: true },
+  version: { type: Number, required: true },
+  public_key_length: { type: Number, required: true },
 });
 
 const Certificate = model<ICertificate>("Certificate", schema);
