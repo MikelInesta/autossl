bfc0d662b7bd8c7d1cedeeaa75794b93ffb94575
MikelInestaUsal <mikel_inesta@usal.es>
2024-03-10 17:42:58 -0600

    Page that shows server, etc... lists

---
+++ client/src/pages/ServersPage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ServerList from "../components/ServerList";
+
+const ServersPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Servers</h1>
+      <ServerList />
+    </div>
+  );
+};
+
+export default ServersPage;
