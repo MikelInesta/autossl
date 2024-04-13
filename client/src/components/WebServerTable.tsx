28be32c565af3b71819e6769a8fc9c5b60552b05
MikelInestaUsal <mikel_inesta@usal.es>
2024-04-13 12:27:10 +0200

    Web servers table

---
+++ client/src/components/WebServerTable.tsx
@@ -0,0 +1,76 @@
+import React, { useEffect, useState } from 'react';
+import {
+	Table,
+	TableBody,
+	TableCell,
+	TableContainer,
+	TableHead,
+	TableRow,
+	Paper,
+} from '@mui/material';
+import WebServerOptionsMenu from './WebServerOptionsMenu';
+
+interface IWebServer {
+	_id: string;
+	web_server_name: string;
+	configuration_path: string;
+	server_id: string;
+	old: boolean;
+}
+
+const WebServerTable: React.FC<{ serverId: string }> = ({
+	serverId,
+}: {
+	serverId: string;
+}) => {
+	const [webServers, setWebServers] = useState<IWebServer[]>([]);
+
+	useEffect(() => {
+		const fetchWebServers = async () => {
+			try {
+				const response = await fetch(
+					`${import.meta.env.VITE_API_URL}/web-servers/serverid/${serverId}`
+				);
+				const data = await response.json();
+				setWebServers(data);
+			} catch (error) {
+				console.error('Error fetching web servers:', error);
+			}
+		};
+
+		fetchWebServers();
+	}, []);
+
+	return (
+		<TableContainer component={Paper}>
+			<Table>
+				<TableHead>
+					<TableRow>
+						<TableCell>ID</TableCell>
+						<TableCell>Name</TableCell>
+						<TableCell>IP Address</TableCell>
+						<TableCell>Operating System</TableCell>
+						<TableCell>Archived (Old)</TableCell>
+						<TableCell>Actions</TableCell>
+					</TableRow>
+				</TableHead>
+				<TableBody>
+					{webServers.map((webServer) => (
+						<TableRow key={webServer._id}>
+							<TableCell>{webServer._id}</TableCell>
+							<TableCell>{webServer.web_server_name}</TableCell>
+							<TableCell>{webServer.configuration_path}</TableCell>
+							<TableCell>{webServer.server_id}</TableCell>
+							<TableCell>{webServer.old ? 'Yes' : 'No'}</TableCell>
+							<TableCell>
+								<WebServerOptionsMenu webServerId={webServer._id} />
+							</TableCell>
+						</TableRow>
+					))}
+				</TableBody>
+			</Table>
+		</TableContainer>
+	);
+};
+
+export default WebServerTable;
