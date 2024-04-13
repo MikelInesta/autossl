635ac98a855dfdb1e8c5a59d179b06b6ea4ef473
MikelInestaUsal <mikel_inesta@usal.es>
2024-04-13 10:58:49 +0200

    Basic homepage

---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,17 @@
+import { Box } from "@mui/system";
+
+const Home = () => {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        height: "100vh",
+      }}
+    >
+      <h1>Home</h1>
+    </Box>
+  );
+};
+
+export default Home;
