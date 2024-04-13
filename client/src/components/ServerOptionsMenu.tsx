f40ea352e583291da10c7032083937d138cea46f
MikelInestaUsal <mikel_inesta@usal.es>
2024-04-13 12:26:12 +0200

    fixed serverId instead of server_ip

--- client/src/components/ServerOptionsMenu.tsx
+++ client/src/components/ServerOptionsMenu.tsx
@@ -2,120 +2,121 @@
   Notes:
     - More options menu could lead to a configuration page for the servers,
       with its agent configuration, delete option, etc...
+		- Maybe I could use pass menu options through props to reuse a optionsmenu component
 */
 
-import * as React from "react";
-import { styled, alpha } from "@mui/material/styles";
-import Button from "@mui/material/Button";
-import Menu, { MenuProps } from "@mui/material/Menu";
-import MenuItem from "@mui/material/MenuItem";
-import Divider from "@mui/material/Divider";
-import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
-import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
-import { Link } from "react-router-dom";
+import * as React from 'react';
+import { styled, alpha } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+import Menu, { MenuProps } from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import Divider from '@mui/material/Divider';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import { Link } from 'react-router-dom';
 
 const StyledMenu = styled((props: MenuProps) => (
-  <Menu
-    elevation={0}
-    anchorOrigin={{
-      vertical: "bottom",
-      horizontal: "right",
-    }}
-    transformOrigin={{
-      vertical: "top",
-      horizontal: "right",
-    }}
-    {...props}
-  />
+	<Menu
+		elevation={0}
+		anchorOrigin={{
+			vertical: 'bottom',
+			horizontal: 'right',
+		}}
+		transformOrigin={{
+			vertical: 'top',
+			horizontal: 'right',
+		}}
+		{...props}
+	/>
 ))(({ theme }) => ({
-  "& .MuiPaper-root": {
-    borderRadius: 6,
-    marginTop: theme.spacing(1),
-    minWidth: 180,
-    color:
-      theme.palette.mode === "light"
-        ? "rgb(55, 65, 81)"
-        : theme.palette.grey[300],
-    boxShadow:
-      "rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px",
-    "& .MuiMenu-list": {
-      padding: "4px 0",
-    },
-    "& .MuiMenuItem-root": {
-      "& .MuiSvgIcon-root": {
-        fontSize: 18,
-        color: theme.palette.text.secondary,
-        marginRight: theme.spacing(1.5),
-      },
-      "&:active": {
-        backgroundColor: alpha(
-          theme.palette.primary.main,
-          theme.palette.action.selectedOpacity
-        ),
-      },
-    },
-  },
+	'& .MuiPaper-root': {
+		borderRadius: 6,
+		marginTop: theme.spacing(1),
+		minWidth: 180,
+		color:
+			theme.palette.mode === 'light'
+				? 'rgb(55, 65, 81)'
+				: theme.palette.grey[300],
+		boxShadow:
+			'rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
+		'& .MuiMenu-list': {
+			padding: '4px 0',
+		},
+		'& .MuiMenuItem-root': {
+			'& .MuiSvgIcon-root': {
+				fontSize: 18,
+				color: theme.palette.text.secondary,
+				marginRight: theme.spacing(1.5),
+			},
+			'&:active': {
+				backgroundColor: alpha(
+					theme.palette.primary.main,
+					theme.palette.action.selectedOpacity
+				),
+			},
+		},
+	},
 }));
 
-const ServerOptionsMenu: React.FC<{ server_ip: string }> = ({
-  server_ip,
+const ServerOptionsMenu: React.FC<{ serverId: string }> = ({
+	serverId,
 }: {
-  server_ip: string;
+	serverId: string;
 }) => {
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-  const open = Boolean(anchorEl);
-  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
+	const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+	const open = Boolean(anchorEl);
+	const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+		setAnchorEl(event.currentTarget);
+	};
+	const handleClose = () => {
+		setAnchorEl(null);
+	};
 
-  return (
-    <div>
-      <Button
-        id="demo-customized-button"
-        aria-controls={open ? "demo-customized-menu" : undefined}
-        aria-haspopup="true"
-        aria-expanded={open ? "true" : undefined}
-        variant="contained"
-        disableElevation
-        onClick={handleClick}
-        endIcon={<KeyboardArrowDownIcon />}
-      >
-        Options
-      </Button>
-      <StyledMenu
-        id="demo-customized-menu"
-        MenuListProps={{
-          "aria-labelledby": "demo-customized-button",
-        }}
-        anchorEl={anchorEl}
-        open={open}
-        onClose={handleClose}
-      >
-        <MenuItem
-          onClick={handleClose}
-          disableRipple
-          component={Link}
-          to={`/servers/${server_ip}/web-servers`}
-        >
-          Web Servers
-        </MenuItem>
-        <MenuItem onClick={handleClose} disableRipple>
-          Domains
-        </MenuItem>
-        <MenuItem onClick={handleClose} disableRipple>
-          Certificates
-        </MenuItem>
-        <Divider sx={{ my: 0.5 }} />
-        <MenuItem onClick={handleClose} disableRipple>
-          <MoreHorizIcon />
-          More
-        </MenuItem>
-      </StyledMenu>
-    </div>
-  );
+	return (
+		<div>
+			<Button
+				id="demo-customized-button"
+				aria-controls={open ? 'demo-customized-menu' : undefined}
+				aria-haspopup="true"
+				aria-expanded={open ? 'true' : undefined}
+				variant="contained"
+				disableElevation
+				onClick={handleClick}
+				endIcon={<KeyboardArrowDownIcon />}
+			>
+				Options
+			</Button>
+			<StyledMenu
+				id="demo-customized-menu"
+				MenuListProps={{
+					'aria-labelledby': 'demo-customized-button',
+				}}
+				anchorEl={anchorEl}
+				open={open}
+				onClose={handleClose}
+			>
+				<MenuItem
+					onClick={handleClose}
+					disableRipple
+					component={Link}
+					to={`/servers/${serverId}/web-servers`}
+				>
+					Web Servers
+				</MenuItem>
+				<MenuItem onClick={handleClose} disableRipple>
+					Domains
+				</MenuItem>
+				<MenuItem onClick={handleClose} disableRipple>
+					Certificates
+				</MenuItem>
+				<Divider sx={{ my: 0.5 }} />
+				<MenuItem onClick={handleClose} disableRipple>
+					<MoreHorizIcon />
+					More
+				</MenuItem>
+			</StyledMenu>
+		</div>
+	);
 };
 
 export default ServerOptionsMenu;
