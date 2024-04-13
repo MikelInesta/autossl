7c628b7cc49162e6d1ecccab7f9b5dc32adb584e
MikelInestaUsal <mikel_inesta@usal.es>
2024-04-13 12:26:55 +0200

    Removed "show servers" button

--- client/src/pages/Servers.tsx
+++ client/src/pages/Servers.tsx
@@ -1,40 +1,24 @@
-import * as React from "react";
-import ServerTable from "../components/ServerTable";
-import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
+import * as React from 'react';
+import ServerTable from '../components/ServerTable';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 
 const Servers: React.FC = () => {
-  const [showServerTable, setShowServerTable] = React.useState(false);
+	const [showServerTable, setShowServerTable] = React.useState(false);
 
-  return (
-    <>
-      <Box
-        sx={{
-          display: "flex",
-          justifyContent: "center",
-        }}
-      >
-        <h1>Servers</h1>
-      </Box>
-      {showServerTable && <ServerTable />}
-      {!showServerTable && (
-        <Box
-          sx={{
-            display: "flex",
-            justifyContent: "center",
-            marginTop: 2,
-          }}
-        >
-          <Button
-            variant="contained"
-            onClick={() => setShowServerTable(!showServerTable)}
-          >
-            Display Servers
-          </Button>
-        </Box>
-      )}
-    </>
-  );
+	return (
+		<>
+			<Box
+				sx={{
+					display: 'flex',
+					justifyContent: 'center',
+				}}
+			>
+				<h1>Servers</h1>
+			</Box>
+			<ServerTable />
+		</>
+	);
 };
 
 export default Servers;
