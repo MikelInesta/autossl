cd321a16bc3e1f0e86be3f445431bf794295922e
MikelInestaUsal <mikel_inesta@usal.es>
2024-04-13 12:28:03 +0200

    Title goes above id

--- client/src/pages/WebServers.tsx
+++ client/src/pages/WebServers.tsx
@@ -1,24 +1,23 @@
-import * as React from "react";
-import Box from "@mui/material/Box";
-import { useParams } from "react-router-dom";
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import { useParams } from 'react-router-dom';
+import WebServerTable from '../components/WebServerTable';
 
 const WebServers: React.FC = () => {
-  const { serverId } = useParams();
+	const { serverId } = useParams();
 
-  React.useEffect(() => {}, [serverId]);
-  return (
-    <>
-      <Box
-        sx={{
-          display: "flex",
-          justifyContent: "center",
-        }}
-      >
-        <h1>WebServers</h1>
-        <h2>Server ID: {serverId}</h2>
-      </Box>
-    </>
-  );
+	React.useEffect(() => {}, [serverId]);
+	return (
+		<>
+			<Box sx={{ display: 'flex', justifyContent: 'center' }}>
+				<h1>Web Servers</h1>
+			</Box>
+			<Box sx={{ display: 'flex', justifyContent: 'center', marginTop: 0 }}>
+				<h2>Server ID: {serverId}</h2>
+			</Box>
+			{serverId && <WebServerTable serverId={serverId} />}
+		</>
+	);
 };
 
 export default WebServers;
