b4394eaa7b3c57b74fe61cd76f8ad74cd3dc6009
MikelInestaUsal <mikel_inesta@usal.es>
2024-04-14 10:25:40 +0200

    Virtual hosts table

---
+++ client/src/pages/VirtualHosts.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import { useParams } from 'react-router-dom';
+import VirtualHostTable from '../components/VirtualHostTable';
+
+const VirtualHosts: React.FC = () => {
+	const { serverId, webServerId } = useParams();
+
+	React.useEffect(() => {}, [serverId]);
+	return (
+		<>
+			<Box sx={{ display: 'flex', justifyContent: 'center' }}>
+				<h1>Web Servers</h1>
+			</Box>
+			<Box sx={{ display: 'flex', justifyContent: 'center', marginTop: 0 }}>
+				<h2>Server ID: {serverId}</h2>
+				<h3>Web Server ID: {webServerId}</h3>
+			</Box>
+			{serverId && webServerId && (
+				<VirtualHostTable serverId={serverId} webServerId={webServerId} />
+			)}
+		</>
+	);
+};
+
+export default VirtualHosts;
