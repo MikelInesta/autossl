046d5e2f5a04ddc3f701b4d4b98c545d48019959
MikelInestaUsal <mikel_inesta@usal.es>
2024-04-14 10:27:08 +0200

    If there is no specific web server to show show them all

--- client/src/pages/WebServers.tsx
+++ client/src/pages/WebServers.tsx
@@ -2,11 +2,31 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import { useParams } from 'react-router-dom';
 import WebServerTable from '../components/WebServerTable';
+import { IWebServer } from '../types/models';
 
 const WebServers: React.FC = () => {
 	const { serverId } = useParams();
+	const [webServers, setWebServers] = React.useState<IWebServer[]>([]);
 
-	React.useEffect(() => {}, [serverId]);
+	if (serverId)
+		React.useEffect(() => {}, [serverId]); // Render on serverId change
+	else {
+		React.useEffect(() => {
+			const fetchWebServers = async () => {
+				try {
+					const response = await fetch(
+						`${import.meta.env.VITE_API_URL}/servers`
+					);
+					const data = await response.json();
+					setWebServers(data);
+				} catch (error) {
+					console.error('Error fetching servers:', error);
+				}
+			};
+
+			fetchWebServers();
+		}, []);
+	}
 	return (
 		<>
 			<Box sx={{ display: 'flex', justifyContent: 'center' }}>
@@ -16,6 +36,10 @@ const WebServers: React.FC = () => {
 				<h2>Server ID: {serverId}</h2>
 			</Box>
 			{serverId && <WebServerTable serverId={serverId} />}
+			{!serverId &&
+				webServers.map((webServer) => (
+					<WebServerTable serverId={webServer._id} />
+				))}
 		</>
 	);
 };
