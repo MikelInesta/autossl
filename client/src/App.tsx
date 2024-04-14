cc0369ac51c0c6961bb8950d1712b0800a28830f
MikelInestaUsal <mikel_inesta@usal.es>
2024-04-14 10:24:55 +0200

    Added the web-server route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,29 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "./pages/Layout";
-import Home from "./pages/Home";
-import NoPage from "./pages/NoPage";
-import Servers from "./pages/Servers";
-import WebServers from "./pages/WebServers";
-import * as React from "react";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './pages/Layout';
+import Home from './pages/Home';
+import NoPage from './pages/NoPage';
+import Servers from './pages/Servers';
+import WebServers from './pages/WebServers';
+import * as React from 'react';
+import VirtualHosts from './pages/VirtualHosts';
 
 const App: React.FC = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="servers" element={<Servers />} />
-          <Route path="/servers/:serverId/web-servers" Component={WebServers} />
-          <Route path="*" element={<NoPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Layout />}>
+					<Route index element={<Home />} />
+					<Route path="servers" element={<Servers />} />
+					<Route path="/servers/:serverId/web-servers" Component={WebServers} />
+					<Route
+						path="/servers/:serverId/web-servers/:webServerId/domains"
+						Component={VirtualHosts}
+					/>
+					<Route path="*" element={<NoPage />} />
+				</Route>
+			</Routes>
+		</BrowserRouter>
+	);
 };
 
 export default App;
