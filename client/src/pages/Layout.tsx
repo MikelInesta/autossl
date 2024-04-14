4ff5c42c925a2aef2bb63438625a649af2193333
MikelInestaUsal <mikel_inesta@usal.es>
2024-04-14 10:26:27 +0200

    Added a Paper component to the main layout

--- client/src/pages/Layout.tsx
+++ client/src/pages/Layout.tsx
@@ -1,26 +1,31 @@
-import { Link, Outlet } from "react-router-dom";
-import BasicMenu from "../components/BasicMenu";
-import IMAGES from "../images/Images";
+import { Link, Outlet } from 'react-router-dom';
+import BasicMenu from '../components/BasicMenu';
+import IMAGES from '../images/Images';
+import { Paper } from '@mui/material';
 
 const Layout = () => {
-  return (
-    <>
-      <nav>
-        <div style={{ display: "flex", justifyContent: "space-between" }}>
-          <Link to="/">
-            <img
-              src={IMAGES.autosslLogo}
-              alt="AutoSSL"
-              style={{ width: 200, height: "auto", borderRadius: 1 }}
-            />
-          </Link>
-          <BasicMenu />
-        </div>
-      </nav>
-
-      <Outlet />
-    </>
-  );
+	return (
+		<>
+			<nav>
+				<div style={{ display: 'flex', justifyContent: 'space-between' }}>
+					<Link to="/">
+						<img
+							src={IMAGES.autosslLogo}
+							alt="AutoSSL"
+							style={{ width: 200, height: 'auto', borderRadius: 1 }}
+						/>
+					</Link>
+					<BasicMenu />
+				</div>
+			</nav>
+			<Paper
+				elevation={3}
+				sx={{ width: '90%', marginLeft: '5%', marginRight: '5%' }}
+			>
+				<Outlet />
+			</Paper>
+		</>
+	);
 };
 
 export default Layout;
