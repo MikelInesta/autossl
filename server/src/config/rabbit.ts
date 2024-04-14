f0e5cec44c7c545a5fa63fa366dffb943e0bdb20
MikelInestaUsal <mikel_inesta@usal.es>
2024-04-14 23:07:12 +0200

    Testing for message publishing in the exchange

--- server/src/config/rabbit.ts
+++ server/src/config/rabbit.ts
@@ -1,11 +1,11 @@
 import * as amqp from 'amqplib';
 
-const declareExchange = async () => {
+const declareExchange = async (name: string, type:string) => {
     try {
         const connection = await amqp.connect('amqp://localhost');
         const channel = await connection.createChannel();
 
-        await channel.assertExchange("csrExchange", "direct", { durable: true });
+        await channel.assertExchange(name, type, { durable: true });
 
         await channel.close();
         await connection.close();
@@ -15,4 +15,24 @@ const declareExchange = async () => {
     }
 }
 
-export {declareExchange};
+async function publishMessage(exchangeName:string, key:string ,message:any) {
+    try {
+        const connection = await amqp.connect('amqp://localhost');
+        const channel = await connection.createChannel();
+
+        const message = 'Message from the backend.';
+        // Routing key should be the ip address of the server hosting the agent, maybe...
+        await channel.publish(exchangeName, key, Buffer.from(message));
+
+        console.log(`Message published to exchange ${exchangeName}`);
+        
+
+        await channel.close();
+        await connection.close();
+    } catch (error) {
+        console.error('Something went wrong:', error);
+        throw error;
+    }
+}
+
+export {declareExchange, publishMessage};
