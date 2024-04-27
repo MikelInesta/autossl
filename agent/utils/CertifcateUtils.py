0072ddfd09826dadd44a6d02d15240e7e9d10fa9
MikelInestaUsal <mikel_inesta@usal.es>
2024-04-27 18:02:17 +0200

    reading the .pem cert as binary data

--- agent/utils/CertifcateUtils.py
+++ agent/utils/CertifcateUtils.py
@@ -1,24 +1,25 @@
 from .x509Parser import x509Parser
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
-import json
-import base64
 import os
 
 
 class CertificateUtils:
 
+    @staticmethod
     def processCertificate(certificatePath):
-        with open(certificatePath, "r") as file:
+        with open(certificatePath, "rb") as file:
             caBuff = file.read()
         certificate = x509Parser.parse_x509(caBuff, ignore_extensions=True)
         return certificate
 
+    @staticmethod
     def getCsr(domain):
         # Here I should use pulumi or something to actually generate the csr
         # but for now im just testing communication
         return "csr"
 
+    @staticmethod
     def writePrivateKey(path):
         key = rsa.generate_private_key(
             public_exponent=65537,
