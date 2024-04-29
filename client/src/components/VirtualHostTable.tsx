3be49b3dc27c7efb3f01e6ad86772bd82807d25b
MikelInestaUsal <mikel_inesta@usal.es>
2024-04-29 19:17:01 +0200

    removed empty dependencies array

--- client/src/components/VirtualHostTable.tsx
+++ client/src/components/VirtualHostTable.tsx
@@ -1,96 +1,96 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import {
-	Table,
-	TableBody,
-	TableCell,
-	TableContainer,
-	TableHead,
-	TableRow,
-	Paper,
-} from '@mui/material';
-import VirtualHostOptionsMenu from './VirtualHostOptionsMenu';
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+} from "@mui/material";
+import VirtualHostOptionsMenu from "./VirtualHostOptionsMenu";
 
 interface IVirtualHost {
-	_id: string;
-	vh_ips: Array<String>;
-	domain_names: String;
-	enabled: boolean;
-	web_server_id: string;
-	certificate_id: string;
-	old: boolean;
+  _id: string;
+  vh_ips: Array<string>;
+  domain_names: string;
+  enabled: boolean;
+  web_server_id: string;
+  certificate_id: string;
+  old: boolean;
 }
 
 const VirtualHostTable: React.FC<{ serverId: string; webServerId: string }> = ({
-	serverId,
-	webServerId,
+  serverId,
+  webServerId,
 }: {
-	serverId: string;
-	webServerId: string;
+  serverId: string;
+  webServerId: string;
 }) => {
-	const [virtualHosts, setVirtualHosts] = useState<IVirtualHost[]>([]);
+  const [virtualHosts, setVirtualHosts] = useState<IVirtualHost[]>([]);
 
-	useEffect(() => {
-		const fetchVirtualHosts = async () => {
-			try {
-				const response = await fetch(
-					`${
-						import.meta.env.VITE_API_URL
-					}/virtual-hosts/webserverid/${webServerId}`
-				);
-				const data = await response.json();
-				setVirtualHosts(data);
-			} catch (error) {
-				console.error('Error fetching virtual hosts:', error);
-			}
-		};
+  useEffect(() => {
+    const fetchVirtualHosts = async () => {
+      try {
+        const response = await fetch(
+          `${
+            import.meta.env.VITE_API_URL
+          }/virtual-hosts/webserverid/${webServerId}`,
+        );
+        const data = await response.json();
+        setVirtualHosts(data);
+      } catch (error) {
+        console.error("Error fetching virtual hosts:", error);
+      }
+    };
 
-		fetchVirtualHosts();
-	}, []);
+    fetchVirtualHosts();
+  });
 
-	return (
-		<TableContainer component={Paper}>
-			<Table>
-				<TableHead>
-					<TableRow>
-						<TableCell>ID</TableCell>
-						<TableCell>IP Addresses</TableCell>
-						<TableCell>Domain Names</TableCell>
-						<TableCell>Is Enabled</TableCell>
-						<TableCell>Web Server ID</TableCell>
-						<TableCell>Certificate ID</TableCell>
-						<TableCell>Is Old</TableCell>
-						<TableCell>Actions</TableCell>
-					</TableRow>
-				</TableHead>
-				<TableBody>
-					{virtualHosts.map((virtualHost) => (
-						<TableRow key={virtualHost._id}>
-							<TableCell>{virtualHost._id}</TableCell>
-							<TableCell>
-								<ul>
-									{virtualHost.vh_ips.map((ip) => (
-										<li>{ip}</li>
-									))}
-								</ul>
-							</TableCell>
-							<TableCell>{virtualHost.domain_names}</TableCell>
-							<TableCell>{virtualHost.enabled ? 'Yes' : 'No'}</TableCell>
-							<TableCell>{virtualHost.web_server_id}</TableCell>
-							<TableCell>{virtualHost.web_server_id}</TableCell>
-							<TableCell>{virtualHost.old ? 'Yes' : 'No'}</TableCell>
-							<TableCell>
-								<VirtualHostOptionsMenu
-									serverId={serverId}
-									webServerId={virtualHost.web_server_id}
-									virtualHostId={virtualHost._id}
-								/>
-							</TableCell>
-						</TableRow>
-					))}
-				</TableBody>
-			</Table>
-		</TableContainer>
-	);
+  return (
+    <TableContainer component={Paper}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>ID</TableCell>
+            <TableCell>IP Addresses</TableCell>
+            <TableCell>Domain Names</TableCell>
+            <TableCell>Is Enabled</TableCell>
+            <TableCell>Web Server ID</TableCell>
+            <TableCell>Certificate ID</TableCell>
+            <TableCell>Is Old</TableCell>
+            <TableCell>Actions</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {virtualHosts.map((virtualHost) => (
+            <TableRow key={virtualHost._id}>
+              <TableCell>{virtualHost._id}</TableCell>
+              <TableCell>
+                <ul>
+                  {virtualHost.vh_ips.map((ip) => (
+                    <li>{ip}</li>
+                  ))}
+                </ul>
+              </TableCell>
+              <TableCell>{virtualHost.domain_names}</TableCell>
+              <TableCell>{virtualHost.enabled ? "Yes" : "No"}</TableCell>
+              <TableCell>{virtualHost.web_server_id}</TableCell>
+              <TableCell>{virtualHost.web_server_id}</TableCell>
+              <TableCell>{virtualHost.old ? "Yes" : "No"}</TableCell>
+              <TableCell>
+                <VirtualHostOptionsMenu
+                  serverId={serverId}
+                  webServerId={virtualHost.web_server_id}
+                  virtualHostId={virtualHost._id}
+                />
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
 };
 
 export default VirtualHostTable;
--- client/src/components/WebServerTable.tsx
+++ client/src/components/WebServerTable.tsx
@@ -1,78 +1,78 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import {
-	Table,
-	TableBody,
-	TableCell,
-	TableContainer,
-	TableHead,
-	TableRow,
-	Paper,
-	Box,
-} from '@mui/material';
-import WebServerOptionsMenu from './WebServerOptionsMenu';
-import { IWebServer } from '../types/models';
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  Box,
+} from "@mui/material";
+import WebServerOptionsMenu from "./WebServerOptionsMenu";
+import { IWebServer } from "../types/models";
 
 const WebServerTable: React.FC<{ serverId: string }> = ({
-	serverId,
+  serverId,
 }: {
-	serverId: string;
+  serverId: string;
 }) => {
-	const [webServers, setWebServers] = useState<IWebServer[]>([]);
+  const [webServers, setWebServers] = useState<IWebServer[]>([]);
 
-	useEffect(() => {
-		const fetchWebServers = async () => {
-			try {
-				const response = await fetch(
-					`${import.meta.env.VITE_API_URL}/web-servers/serverid/${serverId}`
-				);
-				const data = await response.json();
-				setWebServers(data);
-			} catch (error) {
-				console.error('Error fetching web servers:', error);
-			}
-		};
+  useEffect(() => {
+    const fetchWebServers = async () => {
+      try {
+        const response = await fetch(
+          `${import.meta.env.VITE_API_URL}/web-servers/serverid/${serverId}`,
+        );
+        const data = await response.json();
+        setWebServers(data);
+      } catch (error) {
+        console.error("Error fetching web servers:", error);
+      }
+    };
 
-		fetchWebServers();
-	}, []);
+    fetchWebServers();
+  });
 
-	return (
-		<>
-			<Box sx={{ display: 'flex', justifyContent: 'center', marginTop: 0 }}>
-				<h2>Server ID: {serverId}</h2>
-			</Box>
-			<TableContainer component={Paper}>
-				<Table>
-					<TableHead>
-						<TableRow>
-							<TableCell>ID</TableCell>
-							<TableCell>Name</TableCell>
-							<TableCell>IP Address</TableCell>
-							<TableCell>Operating System</TableCell>
-							<TableCell>Archived (Old)</TableCell>
-							<TableCell>Actions</TableCell>
-						</TableRow>
-					</TableHead>
-					<TableBody>
-						{webServers.map((webServer) => (
-							<TableRow key={webServer._id}>
-								<TableCell>{webServer._id}</TableCell>
-								<TableCell>{webServer.web_server_name}</TableCell>
-								<TableCell>{webServer.configuration_path}</TableCell>
-								<TableCell>{webServer.server_id}</TableCell>
-								<TableCell>{webServer.old ? 'Yes' : 'No'}</TableCell>
-								<TableCell>
-									<WebServerOptionsMenu
-										serverId={serverId}
-										webServerId={webServer._id}
-									/>
-								</TableCell>
-							</TableRow>
-						))}
-					</TableBody>
-				</Table>
-			</TableContainer>
-		</>
-	);
+  return (
+    <>
+      <Box sx={{ display: "flex", justifyContent: "center", marginTop: 0 }}>
+        <h2>Server ID: {serverId}</h2>
+      </Box>
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>ID</TableCell>
+              <TableCell>Name</TableCell>
+              <TableCell>IP Address</TableCell>
+              <TableCell>Operating System</TableCell>
+              <TableCell>Archived (Old)</TableCell>
+              <TableCell>Actions</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {webServers.map((webServer) => (
+              <TableRow key={webServer._id}>
+                <TableCell>{webServer._id}</TableCell>
+                <TableCell>{webServer.web_server_name}</TableCell>
+                <TableCell>{webServer.configuration_path}</TableCell>
+                <TableCell>{webServer.server_id}</TableCell>
+                <TableCell>{webServer.old ? "Yes" : "No"}</TableCell>
+                <TableCell>
+                  <WebServerOptionsMenu
+                    serverId={serverId}
+                    webServerId={webServer._id}
+                  />
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
+  );
 };
 
 export default WebServerTable;
