b131ffbd1f9613020cbced8e31824c9d76f96237
MikelInestaUsal <mikel_inesta@usal.es>
2024-05-01 09:49:31 +0200

    Added some random text to the homepage so it does not feel as empty

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,16 +1,34 @@
 import { Box } from '@mui/system';
+import List from "@mui/material/List"
+import ListItem from "@mui/material/ListItem"
 
 const Home = () => {
-	return (
-		<Box
-			sx={{
-				display: 'flex',
-				justifyContent: 'center',
-			}}
-		>
-			<h1>Home</h1>
-		</Box>
-	);
+  return (
+    <>
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+        }}
+      >
+        <h1>Home</h1>
+
+      </Box>
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'center'
+        }}
+      >
+        <p>Pending implementation:</p>
+        <List>
+          <ListItem>Agent security identification and encryption with the backend</ListItem>
+          <ListItem>User management (log-in, owned servers, etc)</ListItem>
+          <ListItem>Encryption of the CSR</ListItem>
+        </List>
+      </Box>
+    </>
+  );
 };
 
 export default Home;
