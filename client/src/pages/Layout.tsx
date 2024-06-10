6f624caba6ff1b4332dbb0c68b585397d5f974fa
MikelInestaUsal <mikel_inesta@usal.es>
2024-06-10 17:35:50 +0200

    Centered Layout

--- client/src/pages/Layout.tsx
+++ client/src/pages/Layout.tsx
@@ -16,23 +16,32 @@ const theme = createTheme({
 const Layout = () => {
   return (
     <ThemeProvider theme={theme}>
-      <Grid container spacing={2} direction={"column"}>
-        <Grid item marginBottom={4}>
-          {/*<OldNavBar />*/}
-          <ResponsiveAppBar />
-        </Grid>
-        <Grid item>
-          <Paper
-            elevation={3}
-            square={true}
-            sx={{
-              ":hover": {
-                boxShadow: 10,
-              },
-            }}
-          >
-            <Outlet />
-          </Paper>
+      <Grid
+        container
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Grid item xs={12} sm={10} md={8} lg={6}>
+          <Grid container spacing={2} direction={"column"}>
+            <Grid item marginBottom={4}>
+              {/*<OldNavBar />*/}
+              <ResponsiveAppBar />
+            </Grid>
+            <Grid item>
+              <Paper
+                elevation={3}
+                square={true}
+                sx={{
+                  ":hover": {
+                    boxShadow: 10,
+                  },
+                }}
+              >
+                <Outlet />
+              </Paper>
+            </Grid>
+          </Grid>
         </Grid>
       </Grid>
     </ThemeProvider>
