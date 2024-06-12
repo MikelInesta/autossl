d89c3096064925765602ef64936d9a5da768bd61
MikelInestaUsal <mikel_inesta@usal.es>
2024-06-12 23:37:28 +0200

    A couple routes to get virtual hosts and certificates for a domain

--- server/src/routes/domains.ts
+++ server/src/routes/domains.ts
@@ -1,11 +1,63 @@
 import express from "express";
 import { Domain } from "../models/domains";
-import { Certificate } from "../models/certificates";
 import { VirtualHost } from "../models/virtual_hosts";
-import { isValidObjectId } from "mongoose";
 import { updateCertificates } from "../controllers/domain";
+import { Certificate } from "../models/certificates";
 
 const domainRouter = express.Router();
+
+/* 
+  Returns the certificates associated to the domain with this domain_names
+*/
+domainRouter.get("/certificates/:domainNames", async (req, res) => {
+  try {
+    const domainNames = req.params.domainNames;
+    if (!domainNames) {
+      res.sendStatus(404);
+      return;
+    }
+
+    const domain = await Domain.findOne({ domain_names: domainNames });
+
+    if (!domain) {
+      res.sendStatus(404);
+      return;
+    }
+
+    let certificates = [];
+
+    for (const certificateId in domain.certificate_ids) {
+      const cert = await Certificate.findById(certificateId);
+      certificates.push(cert);
+    }
+  } catch (e: any) {
+    console.log(
+      `Something went wrong retrieving the certificates: ${e.message}`
+    );
+    res.sendStatus(500);
+  }
+});
+
+/* 
+  Returns the virtual hosts with the given domain names
+*/
+domainRouter.get("/virtual-hosts/:domainNames", async (req, res) => {
+  try {
+    const domainNames = req.params.domainNames;
+    if (!domainNames) {
+      res.sendStatus(404);
+      return;
+    }
+
+    const virtualHosts = await VirtualHost.find({ domain_names: domainNames });
+    res.status(200).send(virtualHosts);
+  } catch (e: any) {
+    console.log(
+      `Something went wrong retrieving a domains virtual hosts: ${e.message}`
+    );
+    res.sendStatus(500);
+  }
+});
 
 domainRouter.get("/:domainNames", async (req, res) => {
   const domainNames = req.params.domainNames;
